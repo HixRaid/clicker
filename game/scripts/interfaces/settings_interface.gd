fefdbf3fb5ae66049429af3e33bfb87ea2de00ae
HixRaid <qqwweerrttsava@gmail.com>
2024-04-28 23:00:45 +0300

    Created control_panel.tscn, improvements_panel.tscn, shop_panel.tscn

--- game/scripts/interfaces/settings_interface.gd
+++ game/scripts/interfaces/settings_interface.gd
@@ -1,8 +1,8 @@
 extends "res://game/scripts/interfaces/interface.gd"
 
-onready var language_option_button = $Panel/SettingsControl/LanguagePanel/OptionButton
-onready var scale_option_button = $Panel/SettingsControl/ScalePanel/OptionButton
-onready var ui_control = $"%Control"
+onready var language_option_button = $Panel/Wrapper/SettingsControl/LanguagePanel/OptionButton
+onready var scale_option_button = $Panel/Wrapper/SettingsControl/ScalePanel/OptionButton
+onready var ui_control = $"%UI"
 
 func _ready():
 	if OS.has_feature("mobile") or OS.has_feature("web") and JavaScript.eval("/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)", true):
