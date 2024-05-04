a2dee2c59dcb7ec1b9a636ab9116d35f9a2624cb
HixRaid <qqwweerrttsava@gmail.com>
2024-05-04 21:43:30 +0300

    Added TopPanel animations

--- game/scripts/ui/control_panel.gd
+++ game/scripts/ui/control_panel.gd
@@ -3,6 +3,7 @@ extends Control
 var is_active = true
 
 onready var animation_player = $AnimationPlayer
+onready var top_panel = $"../TopPanel"
 onready var improvements_panel = $"../ImprovementsPanel"
 onready var shop_panel = $"../ShopPanel"
 
@@ -10,6 +11,7 @@ onready var shop_panel = $"../ShopPanel"
 func _on_set_visible_shop_panel():
 	is_active = !is_active
 	set_visible_info(is_active)
+	top_panel.set_visible_panel(is_active)
 	improvements_panel.set_visible_panel(is_active)
 	shop_panel.set_visible_panel(!is_active)
 
