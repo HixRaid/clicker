f4540e045619b45ba95cbda54ddb8213c93f3abd
HixRaid <qqwweerrttsava@gmail.com>
2024-05-10 23:23:25 +0300

    Added click animation

--- game/scripts/ui/character_image.gd
+++ game/scripts/ui/character_image.gd
@@ -1,6 +1,21 @@
 extends "res://game/scripts/ui/panel_visibility_switch.gd"
 
+onready var tween = $Tween
+
 
 func set_visible(value):
 	rect_pivot_offset = rect_size / 2
 	set_visible_panel(value)
+
+
+func _gui_input(event):
+	if event is InputEventMouseButton and event.pressed == false:
+		tween.interpolate_property(self, "rect_scale",
+		rect_scale, Vector2(0.925, 0.925), 0.075,
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		
+		tween.interpolate_property(self, "rect_scale",
+		Vector2(0.925, 0.9225), Vector2(1, 1), 0.075,
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0.075)
+		
+		tween.start()
