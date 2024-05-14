89f5531953b8cb1f270fd1725bc79989b4ab1a94
HixRaid <qqwweerrttsava@gmail.com>
2024-05-14 23:32:37 +0300

    Updated click info label

--- game/scripts/ui/character_image.gd
+++ game/scripts/ui/character_image.gd
@@ -1,7 +1,10 @@
 extends "res://game/scripts/ui/panel_visibility_switch.gd"
 
+var clickable = true
+
 onready var click_info_label = load("res://game/scenes/ui/click_info_label.tscn") 
 onready var tween = $Tween
+onready var timer = $Timer
 
 
 func set_visible(value):
@@ -10,17 +13,30 @@ func set_visible(value):
 
 
 func _gui_input(event):
-	if event is InputEventMouseButton and event.pressed == false:
-		tween.interpolate_property(self, "rect_scale",
-		rect_scale, Vector2(0.925, 0.925), 0.075,
-		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-		
-		tween.interpolate_property(self, "rect_scale",
-		Vector2(0.925, 0.9225), Vector2(1, 1), 0.075,
-		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0.075)
-		
-		tween.start()
-		
-		var node = click_info_label.instance()
-		add_child(node)
-		node.set_params(event.position, 1)
+	if event is InputEventMouseButton and event.pressed == true and clickable:
+		clickable = false
+		timer.start()
+		animate()
+		create_click_info_label(event.position)
+
+
+func animate():
+	tween.interpolate_property(self, "rect_scale",
+	rect_scale, Vector2(0.925, 0.925), 0.075,
+	Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	
+	tween.interpolate_property(self, "rect_scale",
+	Vector2(0.925, 0.9225), Vector2(1, 1), 0.075,
+	Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0.075)
+	
+	tween.start()
+
+
+func create_click_info_label(position):
+	var node = click_info_label.instance()
+	add_child(node)
+	node.set_params(position, 1)
+
+
+func _on_timeout():
+	clickable = true
--- game/scripts/ui/click_info_label.gd
+++ game/scripts/ui/click_info_label.gd
@@ -7,8 +7,10 @@ onready var tween = $Tween
 
 func set_params(position, value):
 	text = "+%d" % value
-	tween.interpolate_property(self, "rect_position", position, position + Vector2.UP * 30, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	tween.interpolate_property(self, "modulate", Color.white, Color.transparent, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	position -= rect_pivot_offset
+	tween.interpolate_property(self, "rect_position", position, position + Vector2.UP * 80, 0.4, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.interpolate_property(self, "modulate", Color.white, Color.transparent, 0.4, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.interpolate_property(self, "rect_scale", Vector2.ONE * 1.5, Vector2.ONE, 0.4, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
 
 
