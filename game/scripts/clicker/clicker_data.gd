f7948aa9103a09d1f2bf6c82714676033e12c41e
HixRaid <qqwweerrttsava@gmail.com>
2024-06-03 23:54:22 +0300

    Added clicker data

---
+++ game/scripts/clicker/clicker_data.gd
@@ -0,0 +1,22 @@
+extends Node
+
+const LEVELS = [
+	{
+		"name_key": "CHARACTER_1",
+		"description_key": "DESCRIPTION_CHARACTER_1",
+		"image": "character_1.png",
+		"need_score": 1000
+	},
+	{
+		"name_key": "CHARACTER_2",
+		"description_key": "DESCRIPTION_CHARACTER_2",
+		"image": "character_2.png",
+		"need_score": 3000
+	},
+	{
+		"name_key": "CHARACTER_3",
+		"description_key": "DESCRIPTION_CHARACTER_3",
+		"image": "character_3.png",
+		"need_score": 9000
+	},
+]
